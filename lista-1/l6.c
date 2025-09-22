4759c06bbaac1b522cc22458ba99097f05c86030
Diogo Devotte Ferri <109866579+diogodevotte@users.noreply.github.com>
2025-09-22 14:26:52 -0300

    Delete lista-1 directory

--- lista-1/l6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
- 
-int main(){
-    float media,num1,num2;
-    scanf("%f %f",&num1,&num2);
-    media = (num1+num2)/2;
-    printf("%.3f\n",media);
-    return 0;
-}
--- lista-1/l7.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
- 
-int main(){
-    float h,a,vol,raiz=1.732050807568877;
-    scanf("%f %f",&h,&a);
-    vol = (a*a*raiz*h)/2;
-    printf("O VOLUME DA PIRAMIDE E = %.2f METROS CUBICOS\n",vol);
-    return 0;
-}
